c4a72d93fcd23a85415b329c1af276080adb7c67
manitejavarma <manitejavarma@yahoo.com>
2014-09-11 20:16:28 +0530

    Update connect_func.php

--- connect_func.php
+++ connect_func.php
@@ -1,8 +1,8 @@
 <?php
-$db_hostname='mysql.hostinger.in';
-$db_username='u969003719_mani';
-$db_password='varma8824';
-$db_database='u969003719_mani';
+$db_hostname='';   //Your hostname goes here
+$db_username='';	// username goes here
+$db_password='';	//password goes here
+$db_database='';	//database name goes here
 mysql_connect($db_hostname,$db_username,$db_password)  or die(mysql_error());
 
 mysql_select_db($db_database) or die(mysql_error());
@@ -15,7 +15,7 @@ function sanitize($var)
  return mysql_real_escape_string($var);
  
 }
-function send_mail($email)
+function send_mail($email)     //Not yet used this function anywhere ,you can use it durig signing up
 {
 						$to = "$email";
 						$subject = 'Thanking you';
